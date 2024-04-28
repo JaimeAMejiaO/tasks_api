4f957d3e019e2151fec76acfbf2a17e21053f276
ARJ48 <alejandro.ramirez3@utp.edu.co>
2024-04-28 14:28:30 -0500

    interfaz v1

--- src/app.py
+++ src/app.py
@@ -1,22 +1,116 @@
-from flask import Flask, render_template
-from dotenv import load_dotenv
-import os
+from bson import ObjectId
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+import config.mongodb as dbase
+from task import Task
 
-from config.mongodb import mongo
-from routes.task_routes import task
+# Conexion a la base de datos
+db = dbase.dbConnection()
 
-load_dotenv()
-
+# Creando la aplicación de Flask
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = os.getenv('MONGO_URI')
-mongo.init_app(app)
-
+# Ruta principal
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    tasks = db['tasks']
+    tasks_list = tasks.find()
 
-app.register_blueprint(task, url_prefix='/task')
+    return render_template('index.html', tasks=tasks_list)
 
+
+
+# Ruta para añadir una tarea
+@app.route('/add')
+def view_add_task():
+    # Cargando la vista para añadir una tarea
+    return render_template('add.html')
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    # Obteniendo los datos del formulario
+    title = request.form['title']
+    description = request.form['description']
+    done = 'done' in request.form
+
+    # Creando una nueva tarea
+    if title and description:
+        task = Task(title, description, done)
+        db.tasks.insert_one(task.toCollection())
+        response = jsonify({
+            'message': 'Tarea añadida correctamente',
+            'title': title,
+            'description': description,
+            'done': done
+        })
+    
+        return redirect(url_for('view_add_task'))
+    else:
+        return notFount()
+
+
+
+# Ruta para eliminar tareas
+@app.route('/delete')
+def view_delete_task():
+    tasks = db['tasks']
+    tasks_list = tasks.find()
+
+    # Cargando la vista para eliminar una tarea
+    return render_template('delete.html', tasks=tasks_list)
+
+@app.route('/delete/<id>')
+def delete_task(id):
+    tasks = db['tasks']
+    tasks.delete_one({'_id': ObjectId(id)})
+
+    return redirect(url_for('view_delete_task'))
+
+
+
+# Ruta para actualizar tareas
+@app.route('/update')
+def view_update_task():
+    tasks = db['tasks']
+    tasks_list = tasks.find()
+    # Cargando la vista para actualizar una tarea
+    return render_template('update.html', tasks=tasks_list)
+
+@app.route('/update/<id>', methods=['POST'])
+def update_task(id):
+    # Conexion a la base de datos
+    tasks = db['tasks']
+
+    # Obteniendo los datos del formulario
+    title = request.form['title']
+    description = request.form['description']
+    done = 'done' in request.form
+
+    # Creando una nueva tarea
+    tasks.update_one({'_id': ObjectId(id)}, {'$set': {'title': title, 'description': description, 'done': done}})
+    response = jsonify({
+        'message': 'Tarea actualizada correctamente',
+        'title': title,
+        'description': description,
+        'done': done
+    })
+
+    return redirect(url_for('view_update_task'))
+
+
+
+# Ruta de error 404
+@app.errorhandler(404)
+def notFount(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+    }
+    response = jsonify(message)
+    response.status_code = 404
+    return response
+
+
+
+# Inicializando la aplicación
 if __name__ == '__main__':
-    app.run(debug=True)  
+    app.run(debug=True, port=5000)
--- src/config/mongodb.py
+++ src/config/mongodb.py
@@ -1,3 +1,26 @@
-from flask_pymongo import PyMongo
+from pymongo import MongoClient
+import certifi
+from dotenv import load_dotenv
+import os
 
-mongo = PyMongo()
+# Cargar variables de entorno
+load_dotenv()
+
+# URI de conexión a MongoDB
+MONGO_URI = os.getenv("MONGO_URI")
+# Certificado de conexión
+ca = certifi.where()
+
+# Función para conectar a la base de datos
+def dbConnection():
+    # Conexión a la base de datos
+    try:
+        client = MongoClient(MONGO_URI, tlsCAFile=ca)
+        db = client['db_tasks']
+    # Manejo de error de conexión
+    except ConnectionError:
+        print("Error de conexión")
+
+    # Retornar la conexión
+    return db
+
